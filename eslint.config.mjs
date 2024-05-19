62140de93f27d515f8ed2dde487a9c85e6f0af43
Ashikurjaman <ashikbur007@gmail.com>
2024-05-19 10:49:03 +0600

    lint data position change

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,22 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 export default [
-  {
-    ignores: ["**/node_modules/", ".dist/"],
-    rules: {
-      "no-unused-vars": "error",
-      "no-unused-expressions": "error",
-      "prefer-const": "error",
-      "no-console": "warn",
-      "no-undef": "error",
-    },
-  },
-  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    ignores: ['**/node_modules/', '.dist/'],
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-unused-expressions': 'error',
+      'prefer-const': 'error',
+      'no-console': 'warn',
+      // 'no-undef': 'error',
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+  },
+  { languageOptions: { globals: globals.browser } },
+  // pluginJs.configs.recommended,
+  // ...tseslint.configs.recommended,
 ];
