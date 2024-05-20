45a913c396c27b8c620c8d8099e2f3d28afe9402
unknown <ashikbur007@gmail.com>
2024-05-20 19:44:52 +0600

    joi validate added

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,35 +1,23 @@
 import { Request, Response } from 'express';
 import { studentServices } from './student.service';
-import Joi from 'joi';
+import studentJoiSchema from './student.validate';
 
 const createStudent = async (req: Request, res: Response) => {
   // will send response
   try {
-    const userNameSchema = Joi.object({
-      firstName: Joi.string().trim().required(),
-      middleName: Joi.string().trim(),
-      lastName: Joi.string().trim().required(),
-      // Add any other properties and validation rules you have in the userNameSchema
-    });
-    const studentJoiSchema = Joi.object({
-      id: Joi.string().required(),
-      name: userNameSchema.required(),
-      gender: Joi.string().valid('male', 'female', 'others').required(),
-      contactNo: Joi.string().optional(),
-      emergencyContactNo: Joi.string().optional(),
-      bloodGroup: Joi.string()
-        .valid('A+', 'A-', 'B-', 'B+', 'AB-', 'AB+')
-        .optional(),
-      email: Joi.string().email().required(),
-      avatar: Joi.string().optional(),
-      presentAddress: Joi.string().trim().optional(),
-      paramentAddress: Joi.string().trim().optional(),
-      isActive: Joi.string().valid('Active', 'UnActive').required(),
-    });
-
     const { student: studentData } = req.body;
-    console.log(studentData);
+
+    const { error } = studentJoiSchema.validate(studentData);
     const result = await studentServices.createStudentIntoDb(studentData);
+
+    if (error) {
+      res.status(500).json({
+        success: false,
+        message: 'Something went wrong',
+        error: error.details,
+      });
+    }
+
     res.status(200).json({
       success: true,
       message: 'Student is created successfully',
