a9149253c26ce2d781d1a932f59d9c3f89c89ed3
Ashikurjaman <ashikbur007@gmail.com>
2024-05-21 10:59:13 +0600

    static method added mongoose

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,7 +1,9 @@
 import { StudentModel } from './student.model';
 import { Student } from './student.interface';
 const createStudentIntoDb = async (student: Student) => {
-  const result = await StudentModel.create(student);
+  //const result = await StudentModel.create(student); // builtin static method
+  const students = new StudentModel(student);
+  const result = students.save();
   return result;
 };
 
