93700aaab33b25ef995c30146bf79c82af6763df
unknown <ashikbur007@gmail.com>
2024-06-01 19:38:56 +0600

    zod validation added

--- src/app/modules/users/users.model.ts
+++ src/app/modules/users/users.model.ts
@@ -1,29 +1,37 @@
-import { Schema } from 'mongoose';
+import { model, Schema } from 'mongoose';
 import { TUser } from './users.interface';
 
-const userSchema = new Schema<TUser>({
-  id: {
-    type: String,
-    required: true,
+const userSchema = new Schema<TUser>(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    needsPasswordsChange: {
+      type: Boolean,
+      default: true,
+    },
+    role: {
+      enum: ['student', 'faculty', 'admin'],
+      required: true,
+    },
+    status: {
+      enum: ['in-progress', 'blocked'],
+      required: true,
+      default: 'in-progress',
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
   },
-  password: {
-    type: String,
-    required: true,
+  {
+    timestamps: true,
   },
-  needsPasswordsChange: {
-    type: Boolean,
-    default: true,
-  },
-  role: {
-    enum: ['student', 'faculty', 'admin'],
-    required: true,
-  },
-  status: {
-    enum: ['in-progress', 'blocked'],
-    required: true,
-  },
-  isDeleted: {
-    type: Boolean,
-    default: false,
-  },
-});
+);
+
+export const User = model<TUser>('User', userSchema);
---
+++ src/app/modules/users/users.validate.ts
@@ -0,0 +1,16 @@
+import { optional } from 'joi';
+import { z } from 'zod';
+
+const userValidationSchema = z.object({
+  id: z.string(),
+  password: z
+    .string({
+      invalid_type_error: 'Password Must be string',
+    })
+    .max(20, { message: 'Password can not more then 20 characters' })
+    .optional(),
+});
+
+export const userValidation = {
+  userValidationSchema,
+};
